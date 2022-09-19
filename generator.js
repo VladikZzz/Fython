05bf885fad25676689488889ccdb2bccf713e81d
Vladislav Zharov <71986167+VladikZzz@users.noreply.github.com>
2022-09-19 13:48:53 +0300

    Update generator.js

--- generator.js
+++ generator.js
@@ -12,18 +12,54 @@ async function main() {
     const outputFileName = filename.replace(".ast", ".js");
     await fs.writeFile(outputFileName, jsCode);
     console.log(`Wrote result to ${outputFileName}`)
+
 }
 
 function generate(ast) {
     const results = [];
-    for(let statement of ast) {
-        const line = generateForLine(statement);
+    let types = [];
+    let values = [];
+    let func_names = [];
+    for (let statement of ast) {
+        const line = generateForLine(statement, types, values,func_names);
         results.push(line);
     }
     return results.join("\n");
 }
 
-function generateForLine(line) {
+function generateForLine(line, types, values, func_names) {
+    //if below creates the list of all the functions that exist in .fy file and save them
+    if(!(line === undefined)) {
+        if(!(line.var_value === undefined)) {
+            if(!(line.var_name === undefined)) {
+                func_names.push(line.var_name.value);
+            }
+        }
+    }
+    //in the if below, it checks if the print function, that calls a function:
+    // 1. Calls existing function from our list
+    // 2. Checks if type of argument corresponds with type of function(that is saved in type list)
+    if(!(line === undefined)) {
+        if(line.type === "printFunct") {
+            if(line.arguments[0].type === "funcCall") {
+                if(func_names.includes(line.arguments[0].name.value)) {
+                    if(line.arguments[0].body[0].type === "number") {
+                        if(!(types.includes("Int"))) {
+                            throw new Error("Types are not corresponding");
+                        }
+                    } else if(line.arguments[0].body[0].type === "string") {
+                        if (!(types.includes("String"))) {
+                            throw new Error("Types are not corresponding");
+                        }
+                    } else {
+                        if (!(types.includes("Bool"))) {
+                            throw new Error("Types are not corresponding");
+                        }
+                    }
+                }
+            }
+        }
+    }
     let js = "";
     if(line.type === "var_assign") {
         let varName = undefined;
@@ -69,6 +105,7 @@ function generateForLine(line) {
                 } else {
                     if(!(varOper === undefined)) {
                         js = `var ${varName} = ${varOper.var_value1.value} ${varOper.var_operation.value} ${varOper.var_value2.value}`
+                        console.log(js)
                     } else {
                         js = `var ${varName};`
                     }
@@ -86,10 +123,12 @@ function generateForLine(line) {
                             } else {
                                 js += params[i].param_name.value += ",";
                             }
+                            types.push(params[i].param_type.value);
+                            values.push(params[i].param_name.value);
                         }
                         js += `) {`;
                         let bodyList = line.var_value.body.map((arg) => {
-                            return generateForLine(arg);
+                            return generateForLine(arg, types, values,func_names);
                         });
                         for(let bodyPart of bodyList) {
                             js += `\n${bodyPart}`
@@ -111,7 +150,23 @@ function generateForLine(line) {
                 if(line.arguments[i].type === "funcCall") {
                     js+=line.arguments[i].name.value += "(";
                     for(let j = 0; j < line.arguments[i].body.length; j++) {
-                        js += line.arguments[i].body[j].value + ",";
+                        if(!(line.arguments[i].body[j].value === undefined)) {
+                            js += line.arguments[i].body[j].value + ",";
+                        } else {
+                            if(!(line.arguments[i].body[j].body === undefined)) {
+                                let bodyList = generateForLine(line.arguments[i].body[j], types, values,func_names) + "(";
+                                let l = line.arguments[i].body[j].body.length;
+                                for(let f = 0; f < l; f++) {
+                                    if(f === l - 1) {
+                                        bodyList += line.arguments[i].body[j].body[f].value;
+                                    } else {
+                                        bodyList += line.arguments[i].body[j].body[f].value + ",";
+                                    }
+                                }
+                                bodyList += ")";
+                                js += bodyList;
+                            }
+                        }
                     }
                     js += ")";
                 } else if(line.arguments[i].type === "operation") {
@@ -119,7 +174,7 @@ function generateForLine(line) {
                     if(!(line.arguments[i] === undefined)) {
                         if(line.arguments[i].var_value.length>1) {
                             bodyList1 += line.arguments[i].var_value.map((arg) => {
-                                return generateForLine(arg);
+                                return generateForLine(arg, types, values,func_names);
                             }).join();
                         }
                         console.log(bodyList1);
@@ -135,10 +190,9 @@ function generateForLine(line) {
                     let bodyList1 = "";
                     if(line.arguments[i].var_value.length>1) {
                         bodyList1 += line.var_value.arguments[i].var_value.map((arg) => {
-                            return generateForLine(arg);
+                            return generateForLine(arg, types, values,func_names);
                         }).join();
                     }
-                    console.log(bodyList1);
                 } else {
                     js += `${line.arguments[i].value},`;
                 }
@@ -159,7 +213,7 @@ function generateForLine(line) {
                     if(line.var_value.var_value1.type === "funcCall") {
                         bodyList1 += line.var_value.var_value1.name.value;
                         let bodyVar1 = line.var_value.var_value1.body;
-                        bodyList1 += generateForLine(bodyVar1);
+                        bodyList1 += generateForLine(bodyVar1,types, values,func_names);
                     }
                     else {
                         bodyList1 += line.var_value.var_value1.value;
@@ -167,14 +221,14 @@ function generateForLine(line) {
                     if(line.var_value.var_value2.type === "funcCall") {
                         bodyList2 += line.var_value.var_value2.name.value;
                         let bodyVar2 = line.var_value.var_value2.body;
-                        bodyList2 += generateForLine(bodyVar2);
+                        bodyList2 += generateForLine(bodyVar2,types ,values,func_names);
                     }
                     else {
                         bodyList2 += line.var_value.var_value2.value;
                     }
                     js += `${bodyList1} ${varOper} ${bodyList2};`;
                 } else {
-                    console.log("Error: please enter both values for operation");
+                    console.log("Error:  both values of operation should be of the same type operation");
                 }
             }
         }
@@ -192,7 +246,7 @@ function generateForLine(line) {
             js += `${line.condition.var_value1.value} ${line.condition.var_operation.value} ${line.condition.var_value2.value}) {`
         }
         let bodyList = line.body.map((arg) => {
-            return generateForLine(arg);
+            return generateForLine(arg,types, values,func_names);
         });
         for(let bodyPart of bodyList) {
             js += `\n${bodyPart}`
@@ -204,7 +258,7 @@ function generateForLine(line) {
         js += `for(let ${line.var_name.value} = ${line.var_value1.value};`;
         js += `${line.var_name.value} < ${line.var_value2.value}; ${line.var_name.value}++) {`
         let bodyList = line.body.map((arg) => {
-            return generateForLine(arg);
+            return generateForLine(arg, types, values,func_names);
         });
         for(let bodyPart of bodyList) {
             js += `\n${bodyPart}`
@@ -232,7 +286,7 @@ function generateForLine(line) {
             js += `${line.condition.var_value1.value} ${line.condition.var_operation.value} ${line.condition.var_value2.value}) {`
         }
         let bodyList = line.body.map((arg) => {
-            return generateForLine(arg);
+            return generateForLine(arg,types,values,func_names);
         });
         for(let bodyPart of bodyList) {
             js += `\n${bodyPart}`
@@ -241,7 +295,7 @@ function generateForLine(line) {
         if(!(line.elseBody === undefined)) {
             js += '\nelse {'
             let bodyList1 = line.elseBody.map((arg) => {
-                return generateForLine(arg);
+                return generateForLine(arg,types, values,func_names);
             });
             for(let bodyPart1 of bodyList1) {
                 js += `\n${bodyPart1}`
